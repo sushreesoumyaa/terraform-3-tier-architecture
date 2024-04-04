5ef0aa320b03bb46c5ef5a5308627792f1d20df5
sushreesoumyaa <158253627+sushreesoumyaa@users.noreply.github.com>
2024-04-04 13:23:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,8 +137,8 @@ resource "aws_route_table_association" "b" {
 resource "aws_instance" "webserver1" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  key_name               = "nvj"
+  availability_zone      = "ap-southeast-1"
+  key_name               = "tf2"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
   user_data              = "${file("apache.sh")}"
@@ -151,8 +151,8 @@ resource "aws_instance" "webserver1" {
 resource "aws_instance" "webserver2" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-1b"
-  key_name               = "nvj"
+  availability_zone      = "ap-southeast-1"
+  key_name               = "tf2"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-2.id
   user_data              = "${file("apache.sh")}"
@@ -166,8 +166,8 @@ resource "aws_instance" "webserver2" {
 resource "aws_instance" "appserver1" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  key_name               = "nvj"
+  availability_zone      = "ap-southeast-1"
+  key_name               = "tf2"
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
   subnet_id              = aws_subnet.application-subnet-1.id
   tags = {
@@ -178,8 +178,8 @@ resource "aws_instance" "appserver1" {
 resource "aws_instance" "appserver2" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-1b"
-  key_name               = "nvj"
+  availability_zone      = "ap-southeast-1"
+  key_name               = "tf2"
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
   subnet_id              = aws_subnet.application-subnet-2.id
 
