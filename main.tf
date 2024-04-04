0fc0ea501078b51ff09dd97e3efd3e5299b08844
sushreesoumyaa <158253627+sushreesoumyaa@users.noreply.github.com>
2024-04-04 13:37:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -361,7 +361,7 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "batch4pmdevopswithaws202477"  
+  bucket = "soumya0055"  
 
   acl    = "private"  
   versioning {
@@ -381,5 +381,5 @@ default = ["user1", "user2", "user3", "user4"]
 }
 
 resource "aws_iam_group" "two" {
-name = "devopswithawsbyraham"
+name = "devopswithawsbysoumya"
 }
